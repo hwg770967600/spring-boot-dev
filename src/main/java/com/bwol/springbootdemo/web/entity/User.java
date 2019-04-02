51de9eb14740f4849cce58548e2f79b14c69f070
何文刚 <whewengangbeiwaigonlinecom>
2019-04-02 17:33:40 +0800

    1

--- src/main/java/com/bwol/springbootdemo/web/entity/User.java
+++ src/main/java/com/bwol/springbootdemo/web/entity/User.java
@@ -1,32 +1,28 @@
 package com.bwol.springbootdemo.web.entity;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
+    // 省略构造函数
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    // 省略getter和setter
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
